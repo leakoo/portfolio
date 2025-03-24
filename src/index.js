9922b5bd9f70d0b0cd61ce4d2a0dac67359cb7bb
Dan <139128814+leakoo@users.noreply.github.com>
2025-03-24 23:24:48 +0000

    Rename main.js to index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const menuBtn = document.getElementById("menu-btn");
+const spans = menuBtn.querySelectorAll("span");
+const popup = document.getElementById("popup");
+
+menuBtn.addEventListener("click", () => {
+    spans[0].classList.toggle("menu-open1");
+    spans[1].classList.toggle("menu-open2");
+    spans[2].classList.toggle("menu-open3");
+
+    popup.classList.toggle("opacity-100")
+    popup.classList.toggle("opacity-0")
+    popup.classList.toggle('translate-x-0');
+    popup.classList.toggle('translate-x-full');
+    popup.classList.toggle('pointer-events-none');
+})
