7edd9041fe59d31ffd98b01b5c95263db7745019
Dan <139128814+leakoo@users.noreply.github.com>
2025-03-27 07:04:31 +0000

    Add toggleMenu and toggleMenuTransition functions for cleanercode

--- src/main.js
+++ src/main.js
@@ -3,18 +3,27 @@ import loadParticles from "./particles.js";
 const menuBtn = document.getElementById("menu-btn");
 const spans = menuBtn.querySelectorAll("span");
 const popup = document.getElementById("popup");
+const menuLinks = popup.querySelectorAll("a");
 
-menuBtn.addEventListener("click", () => {
+const toggleMenu = () => {
+  //Change Menu to a X
   spans[0].classList.toggle("menu-open1");
   spans[1].classList.toggle("menu-open2");
   spans[2].classList.toggle("menu-open3");
+};
 
+const toggleMenuTransition = () => {
   //Toggle Menu open transition
   popup.classList.toggle("opacity-100");
   popup.classList.toggle("opacity-0");
   popup.classList.toggle("translate-x-0");
   popup.classList.toggle("translate-x-full");
   popup.classList.toggle("pointer-events-none");
+};
+
+menuBtn.addEventListener("click", () => {
+  toggleMenu();
+  toggleMenuTransition();
 });
 
 loadParticles();
